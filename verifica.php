ddbb47c21dfb6ce1c5391e8ccc58b977f852002f
Kauê <104838003+kauemassuda@users.noreply.github.com>
2023-05-09 15:49:38 -0300

    Create verifica.php

---
+++ verifica.php
@@ -0,0 +1,41 @@
+<?php
+
+// Conectar ao banco de dados MySQL
+$host = "localhost"; // nome do host
+$user = "root"; // nome de usuário do banco de dados
+$pass = ""; // senha do banco de dados
+$db = "daily_tasks"; // nome do banco de dados
+
+$conn = mysqli_connect($host, $user, $pass, $db);
+
+// Verificar se houve erros na conexão
+if (!$conn) {
+    die("Erro ao conectar ao banco de dados: " . mysqli_connect_error());
+}
+
+$email = $_POST["email"];
+$senha = $_POST["senha"];
+
+// Preparar a consulta SQL com parâmetros
+$sql = "SELECT * FROM usuario WHERE Email = ? AND Senha = ?";
+$stmt = mysqli_prepare($conn, $sql);
+mysqli_stmt_bind_param($stmt, "ss", $email, $senha);
+
+// Executar a consulta SQL
+mysqli_stmt_execute($stmt);
+$result = mysqli_stmt_get_result($stmt);
+
+// Verificar se a consulta retornou resultados
+if (mysqli_num_rows($result) > 0) {
+    // Iterar sobre os resultados e exibi-los na tela
+    while($row = mysqli_fetch_assoc($result)) {
+        header("Location: calendarioindex.html");
+    }
+} else {
+    echo "Senha ou email incorreto.";
+}
+
+// Fechar a conexão com o banco de dados
+mysqli_close($conn);
+
+?>
