460a5b69b67dca4ff9d69fb893048fd53b7d1d73
Kauê <104838003+kauemassuda@users.noreply.github.com>
2023-05-19 22:28:49 -0300

    Update senhatrocada.php

--- senhatrocada.php
+++ senhatrocada.php
@@ -1,13 +1,19 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-    <head> <title>inf. de erro</title>
-        <link rel="stylesheet" href="styleinf.css">
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
-    </head>
-    <body> <div class="container">
+<head>
+    <title>Informação de Senha Atualizada</title>
+    <link rel="stylesheet" href="styleinf.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+</head>
+<body>
+    <div class="container">
         <div class="content first-content">
             <div class="first-column">
-                <h2 class="title title-primary"></h2> 
-                <p class="description description-primary">Senha atualizada com sucesso.</p></p>
-            </body>
-    </html>
+                <h2 class="title title-primary"></h2>
+                <p class="description description-primary">Senha atualizada com sucesso.</p>
+                <a href="index.html" class="btn">Voltar para a tela de login</a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
